4ddad249f07636e7d3f36779b758ed2bd71f2594
Roman <roman9900@inbox.ru>
2022-06-30 00:13:01 +0300

    Неправильный вывод, исправился. Теперь ответ на экране эмулятора

--- Practice_29_06_22/app/src/main/java/com/example/practice_29_06_22/SplashActivity.kt
+++ Practice_29_06_22/app/src/main/java/com/example/practice_29_06_22/SplashActivity.kt
@@ -19,12 +19,14 @@ class SplashActivity: Activity() {
     private var editText4: EditText? = null
     private var image: ImageView? = null
     private var button: Button? = null
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         image = findViewById(R.id.iv_funnyPicture)
+        textView = findViewById(R.id.tv_splash)
         editText1 = findViewById(R.id.et_inputWindow_name)
         editText2 = findViewById(R.id.et_inputWindow_height)
         editText3 = findViewById(R.id.et_inputWindow_weight)
@@ -45,15 +47,14 @@ class SplashActivity: Activity() {
         else 0
         val person = Person(name, height, weight, age)
         var value:Double = person.calculateSmth()
-        return if(person.name.length in 1..50 &&
+        return (if(person.name.length in 1..50 &&
             person.height in 1..250 &&
-                person.weight in 1.0..250.0 &&
-                    person.age in 1..150){
-            println("Ответ: $value")
-        }
-        else{
-            println("Данные введены не корректно")
-        }
+            person.weight in 1.0..250.0 &&
+            person.age in 1..150){
+            textView?.setText("Ответ: $value")
+        } else{
+            textView?.setText("Данные введены не корректно")
+        })!!
 
     }
 }
